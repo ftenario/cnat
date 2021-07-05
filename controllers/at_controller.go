1ddb6555bb1f59f581775c4c7f035cf5d4ab2bc0
Ferdinand Enario <ftenario@yahoo.com>
2021-07-05 10:16:25 -0700

    initial commit

---
+++ controllers/at_controller.go
@@ -0,0 +1,154 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	cnatv1alpha1 "cnat/api/v1alpha1"
+	"cnat/pkg/schedule"
+	"cnat/pkg/spawn"
+)
+
+// AtReconciler reconciles a At object
+type AtReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+	Log    logr.Logger
+}
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the At object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
+
+// +kubebuilder:rbac:groups=cnat.ftenario.dev,resources=ats,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=cnat.ftenario.dev,resources=ats/status,verbs=get;update;patch
+func (r *AtReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger := r.Log.WithName("reconciler").WithValues("at", req.NamespacedName)
+	fmt.Println(logger)
+	// logger.Info("---- Reconciling At ------")
+
+	instance := &cnatv1alpha1.At{}
+	err := r.Get(context.TODO(), req.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	if instance.Status.Phase == "" {
+		instance.Status.Phase = cnatv1alpha1.PhasePending
+	}
+
+	//state transition PENDING -> RUNNING -> DONE
+	switch instance.Status.Phase {
+	case cnatv1alpha1.PhasePending:
+		// logger.Info("Phase: PENDING")
+		diff, err := schedule.TimeUntilSchedule(instance.Spec.Schedule)
+		if err != nil {
+			// logger.Error(err, "Schedule parsing failure")
+			return ctrl.Result{}, err
+		}
+		// logger.Info("Schedule parsing done", "Result", fmt.Sprintf("%v", diff))
+		if diff > 0 {
+			//not yet time to execute, wait until schedule time then
+			return ctrl.Result{RequeueAfter: diff * time.Second}, nil
+		}
+
+		// logger.Info("It's time", "Ready to execute", instance.Spec.Command)
+		instance.Status.Phase = cnatv1alpha1.PhaseRunning
+	case cnatv1alpha1.PhaseRunning:
+		// logger.Info("Phase: Running")
+
+		pod := spawn.NewPodForCR(instance)
+		err := ctrl.SetControllerReference(instance, pod, r.Scheme)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		query := &corev1.Pod{}
+		//try to see if the pod already exists
+		err = r.Get(context.TODO(), req.NamespacedName, query)
+		if err != nil && errors.IsNotFound(err) {
+			//does not exists, create a pod
+			err = r.Create(context.TODO(), pod)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+
+			//successfully created a pod
+			// logger.Info("pod create successfully", "name", pod.Name)
+			return ctrl.Result{}, nil
+		} else if err != nil {
+			//requeue with error
+			// logger.Error(err, "cannot create pod")
+			return ctrl.Result{}, err
+		} else if query.Status.Phase == corev1.PodFailed || query.Status.Phase == corev1.PodSucceeded {
+			//pod already finished or error out
+			// logger.Info("container terminated", "reason", query.Status.Reason, "message", query.Status.Message)
+
+		} else {
+			//dont requeue, it will happen automatically when pod status change
+			return ctrl.Result{}, nil
+		}
+	case cnatv1alpha1.PhaseDone:
+		// logger.Info("Phase: Done")
+		//reconcile without requeueing
+		return ctrl.Result{}, nil
+
+	default:
+		// logger.Info("NOP")
+		return ctrl.Result{}, nil
+	}
+
+	//update status
+	err = r.Status().Update(context.TODO(), instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *AtReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	err := ctrl.NewControllerManagedBy(mgr).
+		For(&cnatv1alpha1.At{}).
+		Owns(&corev1.Pod{}).
+		Complete(r)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
